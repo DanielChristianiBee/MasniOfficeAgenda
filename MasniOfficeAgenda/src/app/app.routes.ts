44dc2e5dc33dc8003d96261444a04d4b18fae0e2
Daniel Christiani <daniel.christiani.bee@outlook.com>
2024-10-31 23:29:01 +0100

    homepage html and two components added agenda and add

--- MasniOfficeAgenda/src/app/app.routes.ts
+++ MasniOfficeAgenda/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { AgendaComponent } from './home/agenda/agenda.component';
+import { AddComponent } from './home/add/add.component';
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'agenda', component: AgendaComponent },
+  { path: 'add', component: AddComponent }
 ];
